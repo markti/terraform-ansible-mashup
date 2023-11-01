207874e41934f4d22d27aa5534f7a0b3d0144b36
markti <markti@outlook.com>
2023-11-01 19:23:55 -0400

    add cluster module

--- src/terraform/app/compute.tf
+++ src/terraform/app/compute.tf
@@ -1,15 +1,9 @@
+module "primary" {
 
-module "azure_regions" {
-  # This cannot be dynamic as it is special
-  source   = "../modules/azure-regions"
-  location = azurerm_resource_group.main.location
-}
+  source = "../modules/cluster"
 
-resource "azurerm_orchestrated_virtual_machine_scale_set" "main" {
-
-  name                        = "vmss-${var.application_name}-${var.environment_name}"
-  location                    = azurerm_resource_group.main.location
-  resource_group_name         = azurerm_resource_group.main.name
-  platform_fault_domain_count = module.azure_regions.fault_domain_count
+  name                = "${var.application_name}-${var.environment_name}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.primary_location
 
 }
